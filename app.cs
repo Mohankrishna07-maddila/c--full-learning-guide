d2fc92b533616f0a9d3e90899a734d523d6a1268
manoj <maddilamohankrishna32@gmail.com>
2026-02-04 01:29:25 +0530

    SOLUTION: i clearly understand the same scope error clearly by solving the solution and now it successfully run the program

--- app.cs
+++ app.cs
@@ -40,20 +40,12 @@ else
 
 
 // this is 3rd problem:
-static int ramesh(int a)
+static void manoj(int x)
 {
-    for (int i=0;i<a;i++)
+    for (int i=0;i<x;i++)
     {
-        return i;
+        Console.WriteLine(i);
     }
 }
-
-int a=3;
-if (ramesh(a))
-{
-    Console.WriteLine(a);
-}
-else
-{
-    Console.WriteLine(a);
-}
+int m=3;
+manoj(m);
